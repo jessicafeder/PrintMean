350e9ec7c5094217b78b4605c11a95350f8fe335
Jessica Feder <jessica.feder@student.kyh.se>
2020-09-10 13:21:49 +0200

    test ny kommentar

--- src/PrintMean.java
+++ src/PrintMean.java
@@ -2,6 +2,7 @@
 public class PrintMean {
 
     public static void main(String[] args) {
+        //test
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1,1,3,3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1,2,3,4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1,-2,3,-4));
