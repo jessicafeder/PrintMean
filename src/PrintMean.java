b53fe97a3ec55e73c8bc75b7b66a39e3e7e1742a
Jessica Feder <jessica.feder@student.kyh.se>
2020-09-10 14:52:02 +0200

    new change test branch

--- src/PrintMean.java
+++ src/PrintMean.java
@@ -6,7 +6,7 @@ public class PrintMean {
         // testing
         //testing 33
 
-        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1,1,3,3));
+        System.out.println("Mean of 2, 1, 3, 3: " + meanCalc(2,1,3,3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1,2,3,4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1,-2,3,-4));
         System.out.println("Mean of -111, -11, 24, 2: " + meanCalc(-111,-11,24,2));
